09ef0ca9a1a79c7e562645ad76c8d783ceb2e183
Kakinuma4ko <Kakinuma4ko@gmail.com>
2018-03-17 23:57:06 +0800

    commit by alias

--- ApiTest.py
+++ ApiTest.py
@@ -1,5 +1,10 @@
 import requests
-bgm_api_url = 'https://api.bgm.tv'
-url = bgm_api_url+"/subject/219200"
-res = requests.get(url)
-print res,res.json()
+# bgm_api_url = 'https://api.bgm.tv'
+# url = bgm_api_url+'/subject/219200'+'?responseGroup=large'
+# bgm_json = requests.get(url).json()
+# for ep in bgm_json['eps']:
+#     print ep['airdate'] 
+
+url = 'https://metjm.net/csgo/#S76561198300952829A14185220654D9233155819959642397'
+req = requests.get(url)
+print req.text
--- main.py
+++ main.py
@@ -11,9 +11,13 @@ def hello_world():
 def hello(id=None):
     import requests
     bgm_api_url = 'https://api.bgm.tv'
-    url = bgm_api_url+"/subject/"+id
-    res = requests.get(url)
-    return render_template('bgm.html', id=res.json()['name'])
+    url = bgm_api_url+'/subject/219200'+'?responseGroup=large'
+    bgm_json = requests.get(url).json()
+    bgm_date = []
+    for ep in bgm_json['eps']:
+        bgm_date.append(ep['airdate'])
+    print bgm_date
+    return render_template('bgm.html', con='1')
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
