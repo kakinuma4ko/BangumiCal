897ee13db06dca93d316928d3e498f988a4ffe83
Kakinuma4ko <Kakinuma4ko@gmail.com>
2018-06-22 00:25:31 +0800

    can get page by id, display name

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# BangumiCal
+# BangmiCal
 # http://120.79.234.127:5000/
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask
 from flask import render_template
 
@@ -8,16 +9,18 @@ def hello_world():
     return 'Hello World!'
 
 @app.route('/bgm/<id>')
-def hello(id=None):
+def hello(id=id):
     import requests
     bgm_api_url = 'https://api.bgm.tv'
-    url = bgm_api_url+'/subject/219200'+'?responseGroup=large'
+    url = bgm_api_url+'/subject/'+str(id)+'?responseGroup=large'
     bgm_json = requests.get(url).json()
-    bgm_date = []
-    for ep in bgm_json['eps']:
-        bgm_date.append(ep['airdate'])
-    print bgm_date
-    return render_template('bgm.html', con='1')
+    # print json.dumps(bgm_json, sort_keys=True, indent=4)
+    with open("./test.json",'w') as json_file:
+         json.dump(bgm_json,json_file,ensure_ascii=True)
+    # bgm_date = []
+    # for ep in bgm_json['eps']:
+    #     bgm_date.append(ep['airdate'])
+    return render_template('bgm.html', name=bgm_json['name'])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
