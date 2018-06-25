586cb1851d35729e69155954d92a6d30ae61df14
Kakinuma4ko <Kakinuma4ko@gmail.com>
2018-06-25 17:03:32 +0800

    return ics file,use webcal:// to subscribe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 from flask import Flask
-from flask import render_template
+from flask import render_template, send_file, send_from_directory, make_response
 
 app = Flask(__name__)
 
@@ -23,5 +23,16 @@ def hello(id=id):
     print ep_date_list
     return render_template('bgm.html', name=bgm_json['name'])
 
+@app.route("/ics/<id>", methods=['GET'])
+def download_file(id):
+    # directory = os.getcwd() 
+    file_name = 'kakinuma_test.ics'
+    response = make_response(send_from_directory('./', file_name, as_attachment=True))
+    response.headers["Content-Disposition"] = "attachment; filename={}".format(file_name.encode().decode('latin-1'))
+    print 1
+    return response
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='localhost')
+
+
